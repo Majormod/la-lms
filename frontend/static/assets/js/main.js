46234c2541a14eead08e78564e13a0248ccc7bba
Majormod <your_company_email@example.com>
2025-08-24 14:40:39 +0530

    Main.js Update v4.3.6

--- frontend/static/assets/js/main.js
+++ frontend/static/assets/js/main.js
@@ -1069,7 +1069,7 @@
 
     // ===== START LMS FRONTEND LOGIC (UNIFIED) =====
     eduJs.lmsInit = function () {
-        const API_BASE_URL = 'http://54.221.189.159:5000';
+        const API_BASE_URL = 'http://54.221.189.159';
         const token = localStorage.getItem('lmsToken');
         const user = JSON.parse(localStorage.getItem('lmsUser'));
 
