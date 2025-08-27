b8b42d669e1031032f653cfdd7d579737d0b930c
Majormod <your_company_email@example.com>
2025-08-27 09:17:32 +0530

    Main.js Update v6.4.9

--- frontend/static/assets/js/main.js
+++ frontend/static/assets/js/main.js
@@ -4145,7 +4145,7 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .catch(err => console.error("Delayed navbar update failed:", err));
             
-    }, 500); // 500 milliseconds = 0.5 second delay
+    }, 1500); // 500 milliseconds = 0.5 second delay
 });
 
         handlePageLogic();
