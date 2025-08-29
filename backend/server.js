01a3d2dca0ae79fc4948f5055caff6ff818c7d39
Majormod <your_company_email@example.com>
2025-08-29 23:17:37 +0530

    Server.js Main.js Update v7.9.6

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// Updated v7.9.5
 // This new line explicitly tells the server where to find the .env file
 require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
 const express = require('express');
