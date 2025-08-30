38514a49daba65214bd1ae6fcc022c2b97ddd246
Majormod <your_company_email@example.com>
2025-08-30 11:53:09 +0530

    Main.js Update v8.1.3

--- backend/server.js
+++ backend/server.js
@@ -96,7 +96,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend/static')));
 // when a URL starts with '/uploads'.
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // =================================================================
 // --- API ROUTES ---
