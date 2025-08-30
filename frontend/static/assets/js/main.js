40c395ba160064c22f163ac5e6e039cda5c3c7a5
Majormod <your_company_email@example.com>
2025-08-30 18:23:31 +0530

    Main.js Update v8.4.2

--- frontend/static/assets/js/main.js
+++ frontend/static/assets/js/main.js
@@ -1,3 +1,8 @@
+// --- START OF NEW DEBUGGING CODE ---
+console.log("--- main.js SCRIPT HAS STARTED ---");
+console.log("The current page pathname is:", window.location.pathname);
+// --- END OF NEW DEBUGGING CODE ---
+
 console.log("--- RUNNING LATEST VERSION OF main.js ---");
 (function (window, document, $, undefined) {
     "use strict";
