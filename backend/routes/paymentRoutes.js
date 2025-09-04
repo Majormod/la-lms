02ab2767d32c0cdc47a58d99f5f11f549fe6ab8e
Majormod <your_company_email@example.com>
2025-09-04 22:03:02 +0530

    Main.js+ Update v9.0.2

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth'); // Your JWT auth middleware
+const auth = require('../authMiddleware');
 const { createOrder, verifyPayment } = require('../controllers/paymentController');
 
 // @route   POST api/payment/create-order
