b93c364209ba8b066f372f03328801f46c2b8419
Majormod <your_company_email@example.com>
2025-09-06 18:48:57 +0530

    Main.js+ Update v9.7.0

--- backend/server.js
+++ backend/server.js
@@ -110,7 +110,7 @@ const PORT = process.env.PORT || 5000;
 
 // --- MIDDLEWARE ---
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, '../frontend/static')));
 // when a URL starts with '/uploads'.
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
